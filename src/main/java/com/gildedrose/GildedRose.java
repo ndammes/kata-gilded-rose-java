09e99cb373fb55c042a9364945930b906a304d08
Niklas Dammes <58531076+ndammes@users.noreply.github.com>
2025-10-29 11:22:25 +0000

    Gruppe 2

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,81 @@
 package com.gildedrose;
 
 class GildedRose {
+    private static final String BRIE = "Aged Brie";
+    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
     Item[] items;
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
-    public void updateQuality() {
+    public void updateItems() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+            switch (items[i].name) {
+                case BRIE:
+                    handleBrie(i);
+                    break;
+                case BACKSTAGE:
+                    handleBackstage(i);
+                    break;
+                case SULFURAS:
+                    break;
+                default:
+                    handleOthers(i);
+                    break;
             }
         }
     }
+
+    private void increaseQuality(int index) {
+        if (items[index].quality < 50) {
+            items[index].quality = items[index].quality + 1;
+        }
+    }
+
+    private void reduceQuality(int index) {
+        if (items[index].quality > 0) {
+            items[index].quality = items[index].quality - 1;
+        }
+    }
+
+    private void reduceSellin(int index) {
+        items[index].sellIn = items[index].sellIn - 1;
+    }
+
+    private void handleBrie(int index) {
+        increaseQuality(index);
+        reduceSellin(index);
+
+        if(items[index].sellIn < 0) {
+            increaseQuality(index);
+        }
+    }
+
+    private void handleBackstage(int index) {
+        increaseQuality(index);
+
+        if(items[index].sellIn < 11) {
+            increaseQuality(index);
+        }
+        if(items[index].sellIn < 6) {
+            increaseQuality(index);
+        }
+        
+        
+        reduceSellin(index);
+        if(items[index].sellIn < 0) {
+            items[index].quality = 0;
+        }
+    }
+
+    private void handleOthers(int index) {
+        reduceQuality(index);
+        reduceSellin(index);
+        if(items[index].sellIn < 0) {
+            reduceQuality(index);
+        }
+    } 
 }
